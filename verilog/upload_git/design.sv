b6fadfd2f74ba2a6a52a77eb979fe58b9eec2955
spidy.G <120951553+JayKalt@users.noreply.github.com>
2024-09-19 18:43:19 +0200

    Update files
    Updated:
    ~ design.sv
    ~ testbench.sv

--- verilog/upload_git/design.sv
+++ verilog/upload_git/design.sv
@@ -1,15 +1,18 @@
 module MorraCinese(
 	
+	// Inputs
 	input clk,
 	input[1:0] P1,
 	input[1:0] P2,
 	input START,
 
+	// Outputs
 	output reg[1:0] ROUND = 2'b00,
 	output reg[1:0] GAME = 2'b00
 );
 
 
+	// Registers and variables
 	reg[2:0] CURRENT_STATE = 3'b000;
 	reg[2:0] NEXT_STATE;
 
@@ -24,14 +27,25 @@ module MorraCinese(
 
 
 
+	// FSM Update state
 	always @(posedge clk) begin : UPDATE_STATE
 		CURRENT_STATE = NEXT_STATE;
 	end
 
 
-	// datapath
 
+	// Datapath
 	always @(posedge clk) begin : DATAPATH
+
+	/*	Case 1:
+		If Start signal True or Current state equal to
+		reset state "000", then every value must be setted
+		to its default configuration.
+
+		In case Start signal is True the {P1, P2} bits con-
+		figuration has to be concatenated and added to def-
+		ault value '4' to set the rounds to play.
+	*/
 		if(START || CURRENT_STATE == 3'b000) begin
 			CURRENT_ROUND_WINNER = 2'b00;
 			PREV_ROUND_WINNER = 2'b00;
@@ -41,74 +55,113 @@ module MorraCinese(
 			if(START) begin
 				TO_PLAY = {P1, P2} + 4;
 			end
-		end else begin
 
-			// IF per determinare se ci sono le condizioni per disputare il round
-			if((ADV > 4'b0010 && ADV < 4'b0110) && PLAYED < TO_PLAY) begin
+	/*	Case 2:
+		If we're not in Case 1 it means FSM state is any other
+		but the reset state "000", which are valid state to play
+		a round.
+		So, from now on 2 things could happen:
+			1. We have   valid condition to play the round
+			2. We have invalid condition to play the round
+		So we check condition 1:
+	*/
+		end else if((ADV > 4'b0010 && ADV < 4'b0110) && PLAYED < TO_PLAY) begin
+			
+			/*	If we have a valid condition we have other 2 possible
+				scenarios:
+					1. Round outcome is invalid
+					2. Round outcome is   valid
+			*/
+			// If round outcome is INVALID:
+			if({PREV_ROUND_WINNER, PREV_WINNING_MOVE} == {2'b01, P1} || {PREV_ROUND_WINNER, PREV_WINNING_MOVE} == {2'b10, P2} || P1 == 2'b00 || P2 == 2'b00) begin
+				CURRENT_ROUND_WINNER = 2'b00;
+				PREV_WINNING_MOVE = PREV_WINNING_MOVE;
+				PREV_WINNING_MOVE = PREV_ROUND_WINNER;
 
-				// IF per determinare se il round e' nulla
-				if({PREV_ROUND_WINNER, PREV_WINNING_MOVE} == {2'b01, P1} || {PREV_ROUND_WINNER, PREV_WINNING_MOVE} == {2'b10, P2} || P1 == 2'b00 || P2 == 2'b00) begin
-					CURRENT_ROUND_WINNER = 2'b00;
-					//PREV_ROUND_WINNER = PREV_ROUND_WINNER;
-					PREV_WINNING_MOVE = PREV_WINNING_MOVE;
-
-				// SWITCH-CASE per determinare il vincitore del round
-				end else begin
-					case({P1, P2})
-						// vince 1				
-						4'b0111, 4'b1001, 4'b1110: begin
-							CURRENT_ROUND_WINNER = 2'b01;
-							PREV_ROUND_WINNER = 2'b01;
-							PREV_WINNING_MOVE = P1;
-							ADV = ADV + 1;
+			// If round outcome is   VALID:
+			end else begin
+				case({P1, P2})
+					// Wins P1
+					4'b0111, 4'b1001, 4'b1110: begin
+						CURRENT_ROUND_WINNER = 2'b01;
+						PREV_ROUND_WINNER = 2'b01;
+						PREV_WINNING_MOVE = P1;
+						ADV = ADV + 1;
+					end
+					// Wins P2
+					4'b1101, 4'b0110, 4'b1011: begin
+						CURRENT_ROUND_WINNER = 2'b10;
+						PREV_ROUND_WINNER = 2'b10;
+						PREV_WINNING_MOVE = P2;
+						ADV = ADV - 1;
+					end
+					// Draw
+					4'b0101, 4'b1010, 4'b1111: begin
+						CURRENT_ROUND_WINNER = 2'b11;
+						PREV_ROUND_WINNER = 2'b00;
+						PREV_WINNING_MOVE = 2'b00;
 						end
-						// vince 2
-						4'b1101, 4'b0110, 4'b1011: begin
-							CURRENT_ROUND_WINNER = 2'b10;
-							PREV_ROUND_WINNER = 2'b10;
-							PREV_WINNING_MOVE = P2;
-							ADV = ADV - 1;
-						end
-						// pari
-						4'b0101, 4'b1010, 4'b1111: begin
-							CURRENT_ROUND_WINNER = 2'b11;
-							PREV_ROUND_WINNER = 2'b00;
-							PREV_WINNING_MOVE = 2'b00;
-						end
-					endcase
-					PLAYED = PLAYED + 1;
-				end
+				endcase
+				PLAYED = PLAYED + 1;
 			end
 		end
-		#10;
 	end
 
 
-	// fsm
+
+	// FSM
+
+
+	/* FSM STATE SUMMARY:
+
+	+------------------------------------------------------------------------+
+	|Encode		Name		Description										 |
+	+------------------------------------------------------------------------+
+	|																		 |
+	| 000		START		Reset state, set Datapath to default values		 |
+	| 001		INIT		Initialize state, iff Start signal evaulate True |
+	| 010		WIN1		Round is won by P1								 |
+	| 011		WIN2		Round is won by P2								 |
+	| 100		DRAW		Round draws										 |
+	| 101		INVALID		Round is invalid								 |
+	|																		 |
+	+------------------------------------------------------------------------+
+
+	*/
 
 	always @(posedge clk) begin : FSM
 
-		// Case: reset, in any state
+	/*	Case 1:
+		Start signal evaluates True, no matter the state, the FSM
+		transits to INIT state.
+	*/	
 		if(START) begin
 			NEXT_STATE = 3'b001;
 			ROUND = 2'b00;
 			GAME = 2'b00;
 
-		// Case: no reset, in reset state
-		end else if(!START && CURRENT_STATE == 3'b000) begin
+	/*	Case 2:
+		Start signal evaluates False, if current state is reset state
+		then FSM slef-transits to reset state.
+	*/
+		end else if(CURRENT_STATE == 3'b000) begin
 			NEXT_STATE = 3'b000;
 			ROUND = 2'b00;
 			GAME = 2'b00;
 
-		// Case: no reset, in any other state
-		end else if(!START && CURRENT_STATE > 3'b000) begin
+	/* Case 3:
+		Start signal evaluates False, if current state is any other state
+		but reset state, FSM transits to next state based on the round outcome
+	*/
+		end else begin
 
-			// If: game not done yet
+			ROUND = CURRENT_ROUND_WINNER;
+
+			// If game not done yet
 			if(((ADV > 4'b0010 && ADV < 4'b0110) && PLAYED < TO_PLAY) || PLAYED < 4) begin
-				GAME = 2'b00;						// game currently in play
-				ROUND = CURRENT_ROUND_WINNER;		// round winner gets the round
+				GAME = 2'b00;
 
-				// Case: round winner determines next state
+				// Case to determins the Next State
 				case (ROUND)
 					2'b00: NEXT_STATE = 3'b101;
 					2'b01: NEXT_STATE = 3'b010;
@@ -116,20 +169,17 @@ module MorraCinese(
 					2'b11: NEXT_STATE = 3'b100;
 				endcase
 			
-			// If: game done
+			// If game is done
 			end else begin
-				ROUND = CURRENT_ROUND_WINNER;		// round winner gets the round
-				NEXT_STATE = 3'b000;				// next state goes back to reset state
-
 				// Game winner:
-				if(ADV > 4'b0100) begin				// game won by P1
+				if(ADV > 4'b0100) begin				// Game won by P1
 					GAME = 2'b01;
-				end else if(ADV < 4'b0100) begin	// game won by P2
+				end else if(ADV < 4'b0100) begin	// Game won by P2
 					GAME = 2'b10;
-				end else if(ADV == 4'b0100) begin	// game draws
+				end else if(ADV == 4'b0100) begin	// Game draws
 					GAME = 2'b11;
 				end
-
+				NEXT_STATE = 3'b000;
 			end
 		end
 	end
--- verilog/upload_git/testbench.sv
+++ verilog/upload_git/testbench.sv
@@ -1,118 +1,212 @@
-// Code your testbench here
-// or browse Examples
-module MorraCinese_TB;
-    reg [1:0] P1;
-    reg [1:0] P2;
-    reg START;
-    reg [1:0] ROUND;
-    reg [1:0] GAME;
-    reg clk;
+`timescale 1ns / 1ps
 
-    integer out;
-    integer script;
+module tb_MorraCinese;
 
-  MorraCinese test14 (
-    .P1(P1),
-    .P2(P2),
-    .START(START),
-    .clk(clk),
-    .ROUND(ROUND),
-    .GAME(GAME)
-    );
+	// File descriptors
+	integer tbf, outf;
 
-    task Simulate();
-      $fdisplay(script, "simulate %b %b %b %b %b", P1[1], P1[0], P2[1], P2[0], START);
-    endtask
+	reg clk;
+	reg START;
+	reg [1:0] P1;
+	reg [1:0] P2;
+	reg [1:0] ROUND;
+	reg [1:0] GAME;
 
-    task Output();   
-      $fdisplay(out, "Outputs: %b %b %b %b", ROUND[1],ROUND[0],GAME[1], GAME[0]);
-      endtask
+	MorraCinese testing(
+		clk,
+		P1, P2, START,
+		ROUND, GAME);
+
+	always #10 clk = ~clk;
+
+	initial begin
+		$dumpfile("dump.vdc");
+		$dumpvars(1);
+		tbf = $fopen("testbench.script", "w");
+		outf = $fopen("output_verilog.txt", "w");
+		$fdisplay(tbf,"read_blif FSMD.blif");
+
+		clk = 1'b0;
+
+		//1. Game 1, Round 0
+		START = 1'b1;	
+		P1 = 2'b00;
+		P2 = 2'b00;
+		$fdisplay(tbf, "Simulate %b %b %b", P1, P2, START);			 
+		#20;
+		$fdisplay(outf, "Outputs: %b %b", ROUND, GAME);
+
+		//2. Game 1, Round 1
+		START = 1'b0;
+		P1 = 2'b01;
+		P2 = 2'b01;
+		$fdisplay(tbf, "Simulate %b %b %b", P1, P2, START);			 
+		#20;
+		$fdisplay(outf, "Outputs: %b %b", ROUND, GAME);
+
+		//3. Game 1, Round 2
+		P1 = 2'b10;
+		P2 = 2'b10;
+		$fdisplay(tbf, "Simulate %b %b %b", P1, P2, START);			 
+		#20;
+		$fdisplay(outf, "Outputs: %b %b", ROUND, GAME);
+
+		//4. Game 1, Round 3
+		P1 = 2'b11;
+		P2 = 2'b11;
+		$fdisplay(tbf, "Simulate %b %b %b", P1, P2, START);			 
+		#20;
+		$fdisplay(outf, "Outputs: %b %b", ROUND, GAME);
+
+		//5. Game 1, Round 4
+		P1 = 2'b00;
+		P2 = 2'b00;
+		$fdisplay(tbf, "Simulate %b %b %b", P1, P2, START);			 
+		#20;
+		$fdisplay(outf, "Outputs: %b %b", ROUND, GAME);
+
+		//6. Stand by
+		P1 = 2'b01;
+		P2 = 2'b10;
+		$fdisplay(tbf, "Simulate %b %b %b", P1, P2, START);			 
+		#20;
+		$fdisplay(outf, "Outputs: %b %b", ROUND, GAME);
+
+		//7. Stand by
+		P1 = 2'b01;
+		P2 = 2'b01;
+		$fdisplay(tbf, "Simulate %b %b %b", P1, P2, START);			 
+		#20;
+		$fdisplay(outf, "Outputs: %b %b", ROUND, GAME);
+
+		//8. Game 2, Round 0
+		START = 1'b1;
+		P1 = 2'b00;
+		P2 = 2'b00;
+		$fdisplay(tbf, "Simulate %b %b %b", P1, P2, START);			 
+		#20;
+		$fdisplay(outf, "Outputs: %b %b", ROUND, GAME);
+
+		//9. Game 2, Round 0
+		START = 1'b1;
+		P1 = 2'b00;
+		P2 = 2'b01;
+		$fdisplay(tbf, "Simulate %b %b %b", P1, P2, START);			 
+		#20;
+		$fdisplay(outf, "Outputs: %b %b", ROUND, GAME);
+
+		//10. Game 2, Round 1
+		START = 1'b0;
+		P1 = 2'b01;
+		P2 = 2'b10;
+		$fdisplay(tbf, "Simulate %b %b %b", P1, P2, START);			 
+		#20;
+		$fdisplay(outf, "Outputs: %b %b", ROUND, GAME);
+
+		//11. Game 2, Round 2
+		P1 = 2'b01;
+		P2 = 2'b11;
+		$fdisplay(tbf, "Simulate %b %b %b", P1, P2, START);			 
+		#20;
+		$fdisplay(outf, "Outputs: %b %b", ROUND, GAME);
+
+		//12. Game 2, Round 3
+		P1 = 2'b10;
+		P2 = 2'b01;
+		$fdisplay(tbf, "Simulate %b %b %b", P1, P2, START);			 
+		#20;
+		$fdisplay(outf, "Outputs: %b %b", ROUND, GAME);
+
+		//13. Game 2, Round 4
+		P1 = 2'b11;
+		P2 = 2'b11;
+		$fdisplay(tbf, "Simulate %b %b %b", P1, P2, START);			 
+		#20;
+		$fdisplay(outf, "Outputs: %b %b", ROUND, GAME);
+
+		//14. Stand by
+		P1 = 2'b11;
+		P2 = 2'b00;
+		$fdisplay(tbf, "Simulate %b %b %b", P1, P2, START);			 
+		#20;
+		$fdisplay(outf, "Outputs: %b %b", ROUND, GAME);
+
+		//15. Stand by
+		P1 = 2'b10;
+		P2 = 2'b01;
+		$fdisplay(tbf, "Simulate %b %b %b", P1, P2, START);			 
+		#20;
+		$fdisplay(outf, "Outputs: %b %b", ROUND, GAME);
+
+		//16. Stand by
+		P1 = 2'b00;
+		P2 = 2'b00;
+		$fdisplay(tbf, "Simulate %b %b %b", P1, P2, START);			 
+		#20;
+		$fdisplay(outf, "Outputs: %b %b", ROUND, GAME);
+		
+		//9. Game 3, Round 0
+		START = 1'b1;
+		P1 = 2'b00;
+		P2 = 2'b01;
+		$fdisplay(tbf, "Simulate %b %b %b", P1, P2, START);			 
+		#20;
+		$fdisplay(outf, "Outputs: %b %b", ROUND, GAME);
+
+		//10. Game 3, Round 1
+		START = 1'b0;
+		P1 = 2'b10;
+		P2 = 2'b10;
+		$fdisplay(tbf, "Simulate %b %b %b", P1, P2, START);			 
+		#20;
+		$fdisplay(outf, "Outputs: %b %b", ROUND, GAME);
+
+		//11. Game 3, Round 2
+		P1 = 2'b01;
+		P2 = 2'b11;
+		$fdisplay(tbf, "Simulate %b %b %b", P1, P2, START);			 
+		#20;
+		$fdisplay(outf, "Outputs: %b %b", ROUND, GAME);
+
+		//12. Game 3, Round 3
+		P1 = 2'b10;
+		P2 = 2'b01;
+		$fdisplay(tbf, "Simulate %b %b %b", P1, P2, START);			 
+		#20;
+		$fdisplay(outf, "Outputs: %b %b", ROUND, GAME);
+
+		//13. Game 3, Round 4
+		P1 = 2'b11;
+		P2 = 2'b11;
+		$fdisplay(tbf, "Simulate %b %b %b", P1, P2, START);			 
+		#20;
+		$fdisplay(outf, "Outputs: %b %b", ROUND, GAME);
+
+		//14. Stand by
+		P1 = 2'b11;
+		P2 = 2'b00;
+		$fdisplay(tbf, "Simulate %b %b %b", P1, P2, START);			 
+		#20;
+		$fdisplay(outf, "Outputs: %b %b", ROUND, GAME);
+
+		//15. Stand by
+		P1 = 2'b10;
+		P2 = 2'b01;
+		$fdisplay(tbf, "Simulate %b %b %b", P1, P2, START);			 
+		#20;
+		$fdisplay(outf, "Outputs: %b %b", ROUND, GAME);
+
+		//16. Stand by
+		P1 = 2'b00;
+		P2 = 2'b00;
+		$fdisplay(tbf, "Simulate %b %b %b", P1, P2, START);			 
+		#20;
 
 
-
-    always
-    #10 clk = ~clk;
-  
-  initial begin
-    
-    script = $fopen("testbench.script", "w");
-    out = $fopen("output_verilog.txt", "w");
-
-    $dumpfile("tb.vcd");
-    $dumpvars(1);
-    
-    $fdisplay(script,"rl FSMD.blif");
-    
-    clk = 1'b0;
-
-    START = 1'b1;  
-    P1 = 2'b00;
-    P2 = 2'b01;
-    Simulate();         
-    #20;
-    Output();   //1
-
-
-    START = 1'b0;
-    P1 = 2'b01;
-    P2 = 2'b10;
-    Simulate();         
-    #20;
-    Output();   //2
-
-    P1 = 2'b10;
-    P2 = 2'b01;
-    Simulate();     
-    #20;
-    Output();   //3
-
-    P1 = 2'b01;
-    P2 = 2'b10;
-    Simulate();     
-    #20;
-    Output();   //4
-    
-
-    P1 = 2'b10;
-    P2 = 2'b01;
-    Simulate();     
-    #20;
-    Output();   //5
-    
-
-    P1 = 2'b11;
-    P2 = 2'b11;
-    Simulate();         
-    #20;
-    Output();   //6
-    
-
-    P1 = 2'b01;
-    P2 = 2'b10;
-    Simulate();     
-    #20;
-    Output();   //7
-    
-
-    P1 = 2'b00; 
-    P2 = 2'b00;
-    Simulate(); 
-    #20;
-    Output();   //8
-    
-    START = 1'b0;
-    P1 = 2'b01; 
-    P2 = 2'b10;
-    Simulate(); 
-    #20;
-    Output();   //9
-
-    
-    $fdisplay(script, "quit");
-    $fclose(script);
-    $fclose(out);
-    $finish;
-
-
-  end
+		$fdisplay(outf, "Outputs: %b %b", ROUND, GAME);
+		$fdisplay(tbf, "quit");
+		$fclose(tbf);
+		$fclose(outf);
+		$finish;
+	end
 endmodule
